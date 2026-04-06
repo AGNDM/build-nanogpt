8fbd8c4027ec68e57d6e208c1cf5bc659ec5e86b
AGNDM <eiresendeznickolas9@gmail.com>
2026-04-06 17:55:04 +0200

    First pretraining, 1 epoch for fineweb10B dataset

---
+++ cache_env_setup.sh
@@ -0,0 +1,7 @@
+# 1. 设置 Hugging Face 的总缓存目录
+export HF_HOME="/iopsstor/scratch/cscs/tong/share/xianrong_liu/workspace_cache/hf"
+
+# 2. 设置系统临时文件目录（防止处理大规模数据时 /tmp 爆满）
+export TMPDIR="/iopsstor/scratch/cscs/tong/share/xianrong_liu/workspace_cache/tmp"
+export TEMP="/iopsstor/scratch/cscs/tong/share/xianrong_liu/workspace_cache/tmp"
+export TMP="/iopsstor/scratch/cscs/tong/share/xianrong_liu/workspace_cache/tmp"
