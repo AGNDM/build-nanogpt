80ef403cd79b774d757f637d91084ef362afe0de
AGNDM <eiresendeznickolas9@gmail.com>
2026-04-07 18:29:28 +1000

    Bigger Para GPT!

---
+++ submit_nanogpt_bigger.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=nanogpt-train-bigger
+#SBATCH --account=a131
+#SBATCH --time=05:00:00              
+#SBATCH --nodes=1                    
+#SBATCH --ntasks-per-node=1          
+#SBATCH --gpus-per-node=4            
+#SBATCH --cpus-per-task=288
+#SBATCH --output=log_bigger_sys/%x-%j.out           
+#SBATCH --error=log_bigger_sys/%x-%j.err            
+
+cd /iopsstor/scratch/cscs/tong/share/xianrong_liu/build-nanogpt
+
+srun --uenv=pytorch/v2.8.0:v1 --view=default bash -c " \
+    source /iopsstor/scratch/cscs/tong/share/xianrong_liu/.venv/bin/activate && \
+    source /iopsstor/scratch/cscs/tong/share/xianrong_liu/build-nanogpt/cache_env_setup.sh && \
+    python -m torch.distributed.run --standalone --nproc_per_node=4 train_gpt2_bigger.py \
+"
