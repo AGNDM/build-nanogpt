b7b560d9ff904e59f2cf086b5d7fd817901e10da
AGNDM <eiresendeznickolas9@gmail.com>
2026-04-07 09:40:30 +0200

    2.5 epoch training

--- submit_nanogpt_2nodes_8gpus.sh
+++ submit_nanogpt_2nodes_8gpus.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=nanogpt-train-2n8g
 #SBATCH --account=a131
-#SBATCH --time=08:00:00
+#SBATCH --time=04:00:00
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=4
 #SBATCH --cpus-per-task=288
-#SBATCH --output=log_3_epoch/%x-%j.out
-#SBATCH --error=log_3_epoch/%x-%j.err
+#SBATCH --output=log_3_epoch_full/%x-%j.out
+#SBATCH --error=log_3_epoch_full/%x-%j.err
 
 cd /iopsstor/scratch/cscs/tong/share/xianrong_liu/build-nanogpt
 
