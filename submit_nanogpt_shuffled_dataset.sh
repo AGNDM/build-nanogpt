4a5aeb24224b1ce8b82d7a3a3e29f36de095297d
AGNDM <eiresendeznickolas9@gmail.com>
2026-04-08 22:30:05 +1000

    added shuffled version

---
+++ submit_nanogpt_shuffled_dataset.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=nanogpt-train
+#SBATCH --account=a131
+#SBATCH --time=03:00:00              
+#SBATCH --nodes=1                    
+#SBATCH --ntasks-per-node=1          
+#SBATCH --gpus-per-node=4            
+#SBATCH --cpus-per-task=288
+#SBATCH --output=log/%x-%j.out           
+#SBATCH --error=log/%x-%j.err            
+
+cd /iopsstor/scratch/cscs/tong/share/xianrong_liu/build-nanogpt
+
+srun --uenv=pytorch/v2.8.0:v1 --view=default bash -c " \
+    source /iopsstor/scratch/cscs/tong/share/xianrong_liu/.venv/bin/activate && \
+    source /iopsstor/scratch/cscs/tong/share/xianrong_liu/build-nanogpt/cache_env_setup.sh && \
+    python -m torch.distributed.run --standalone --nproc_per_node=4 train_gpt2_shuffled_dataset.py \
+"
